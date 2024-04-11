e1c14ba629e349951bcc19286dd1571550220fac
Yanina Lysukha <yanina.lysukha@gmail.com>
2024-04-11 18:41:10 +0200

    feat: implement getIdGeneratorFunction function

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -189,8 +189,13 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let counter = startFrom;
+  return () => {
+    const result = counter;
+    counter += 1;
+    return result;
+  };
 }
 
 module.exports = {
